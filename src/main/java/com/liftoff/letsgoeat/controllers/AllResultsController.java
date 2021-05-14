dea860c85d8b85854af3cf087afcf2b7a79822e2
Bre Bequette Reuther <67883123+BreBequette@users.noreply.github.com>
2021-05-13 21:34:43 -0500

    Working on parsing API data - committing before trying a different route with GSON

--- src/main/java/com/liftoff/letsgoeat/controllers/AllResultsController.java
+++ src/main/java/com/liftoff/letsgoeat/controllers/AllResultsController.java
@@ -1,6 +1,14 @@
 package com.liftoff.letsgoeat.controllers;
 
+import com.mashape.unirest.http.ObjectMapper;
+import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.http.HttpResponse;
+//import org.apache.tomcat.util.json.JSONParser;
+//import org.apache.tomcat.util.json.ParseException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -8,8 +16,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.reactive.function.client.WebClient;
-import com.mashape.unirest.http.*;
+import com.mashape.unirest.http.JsonNode;
+//import org.json.simple.parser.JSONParser;
+//import org.json.simple.parser.ParseException;
+
+import java.awt.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
@@ -19,20 +34,45 @@ public class AllResultsController {
     @Value("${api.key}")
     private String apiKey;
 
-    @Autowired
-    private WebClient.Builder webClientBuilder;
 
     @GetMapping
     public String showAllResults(Model model) throws UnirestException {
 
+        //make the API call based on user selections
         Unirest.setTimeouts(0, 0);
-        HttpResponse<String> response = Unirest.get("https://api.yelp.com/v3/businesses/search?location=63139&radius=8046&categories=sushi&price=2")
-                .header("Authorization", "Bearer NFmlRdOTMa5OThvvXwRM-Eti-kc8nzSFhpU_y1eajhWETjPhZeLKaVOUunpMiJ3-Bccqqfa5rLgKjeFwa2iVAebG-v4oL8rhAo8otr8NXMOgKmTTr9Muu-olWRqPYHYx")
-                .header("API", "NFmlRdOTMa5OThvvXwRM-Eti-kc8nzSFhpU_y1eajhWETjPhZeLKaVOUunpMiJ3-Bccqqfa5rLgKjeFwa2iVAebG-v4oL8rhAo8otr8NXMOgKmTTr9Muu-olWRqPYHYx")
-                .asString();
+        HttpResponse<JsonNode> response = Unirest.get("https://api.yelp.com/v3/businesses/search?location=63139&radius=8046&categories=sushi&price=2")
+                .header("Authorization", "Bearer " + apiKey)
+                .header("API", "apiKey").asJson();
 
-        model.addAttribute("title", "All Matches");
-        model.addAttribute("response", response.getBody());
+        //Convert JSON response into an arraylist of Objects
+        JSONObject responseBodyJSONObj = response.getBody().getObject();
+        JSONArray resultsJSONArr = responseBodyJSONObj.getJSONArray("businesses");
+        ArrayList<Object> resultsArr = new ArrayList<>();
+
+        if (resultsJSONArr != null) {
+            for (int i=0; i<resultsJSONArr.length(); i++) {
+                resultsArr.add(resultsJSONArr.get(i));
+            }
+        }
+
+        //try to store json in map instead
+
+
+
+        //pull out specific results needed
+        String name="bre";
+        String cuisine;
+        String priceRange;
+        String address;
+        String phone;
+
+
+        //add results to page
+        model.addAttribute("title", "All Results");
+        model.addAttribute("results", resultsArr);
+
+        //trying to access specific pieces of info
+        model.addAttribute("name", name);
         return "all-results";
     }
 }
